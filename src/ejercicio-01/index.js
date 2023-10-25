1cd8f82f060ef747af152df92dc225e00779492d
Fernando <fernando30041972@hotmail.com>
2023-10-25 23:01:00 +0200

    Añadir ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,10 @@
+const nombre = "NOLY"
+
+const edad = 51
+
+const esEStudiante = true
+
+const mensaje = 'Hola, mi nombre es' + nombre + ' y tengo ' + edad +' 51 años. Soy estudiante: ' + esEstudiante + '.' 
+
+console.log('Mensaje:', mensaje)
+
